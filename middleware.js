5d338ee49f3df32ccba3fd3fe41b91414baa8168
Shrey Singhal <shreynbd12@gmail.com>
2025-01-24 16:05:54 +0530

    Export middleware

--- middleware.js
+++ middleware.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
 // Middleware to validate MongoDB ObjectID
-export const validateObjectId = (req, res, next) => {
+const validateObjectId = (req, res, next) => {
     const { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(400).json({ message: 'Invalid ID format' });
     }
     next();
 };
+
+module.exports = {
+    validateObjectId,
+}

c5b16595ef8c1e64107d6f15c34cfd47774fd73f
Shrey Singhal <shreynbd12@gmail.com>
2025-01-24 15:19:14 +0530

    validateObjectId middleware

---
+++ middleware.js
@@ -0,0 +1,8 @@
+// Middleware to validate MongoDB ObjectID
+export const validateObjectId = (req, res, next) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: 'Invalid ID format' });
+    }
+    next();
+};

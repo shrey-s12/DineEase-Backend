4c267c7ecd049b87826ec9ae998f9e4cd546d876
Shrey Singhal <shreynbd12@gmail.com>
2025-01-24 12:12:57 +0530

    Correct Naming Convention of models files

--- model/counterModel.js
+++ model/counterModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 const CounterSchema = new Schema({
     image: { type: String },
     name: { type: String, required: true },
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
-const User = require('../model/user');
+const User = require('../model/userModel');
 
 router.get("/", async (req, res) => {
     try {

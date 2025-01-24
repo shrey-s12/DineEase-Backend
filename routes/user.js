f80b2eaf47ad7540b8ec6c68f5fb0dc345ee0e47
Shrey Singhal <shreynbd12@gmail.com>
2025-01-24 14:36:43 +0530

    Create Get, GetById, Post, Put and Delete userRoutes

--- routes/user.js
+++ routes/user.js
@@ -5,31 +5,82 @@ const User = require('../model/userModel');
 
 router.get("/", async (req, res) => {
     try {
-        const users = await User.find();
-        res.json(users);
+        const users = await User.find().select('-cart -password');
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await User.findById(id).select('-cart -password');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 
 router.post("/", async (req, res) => {
-    const { name, email, password, role } = req.body;
+    const { image, name, email, password, role } = req.body;
     try {
+
+        // Check existingUser
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already in use' });
+        }
+
         const salt = await bcrypt.genSalt();
         const hashedPassword = await bcrypt.hash(password, salt);
-        const newUser = new User({ name, email, password: hashedPassword, role });
+
+        const newUser = new User({ image, name, email, password: hashedPassword, role });
         const user = await newUser.save();
-        res.status(201).json(user);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const updateData = { ...req.body };
+
+    if (updateData.password) {
+        const salt = await bcrypt.genSalt();
+        updateData.password = await bcrypt.hash(updateData.password, salt);
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            id,
+            req.body,
+            { new: true }
+        );
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const user = await updatedUser.save();
+        res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 
 router.delete("/:id", async (req, res) => {
-    const id = req.params.id;
+    const { id } = req.params;
     try {
         const user = await User.findByIdAndDelete(id);
-        res.json(user);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }

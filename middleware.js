f322db3285572d25462da260df97aab4e18b404f
Shrey Singhal <shreynbd12@gmail.com>
2025-01-24 18:33:14 +0530

    Middleware for counterId

--- middleware.js
+++ middleware.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 // Middleware to validate MongoDB ObjectID
 const validateObjectId = (req, res, next) => {
-    const { id } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(id)) {
+    const id = req.params.id || req.params.counterId;
+    if (!mongoose.Types.ObjectId.isValid({ id })) {
         return res.status(400).json({ message: 'Invalid ID format' });
     }
     next();

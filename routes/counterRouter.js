6842293b7e94551053719b4d59e6ad11bba8410d
Shrey Singhal <shreynbd12@gmail.com>
2025-01-24 23:21:57 +0530

    Create Middleware to validate Merchant IDs

--- routes/counterRouter.js
+++ routes/counterRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { validateObjectId } = require('../middleware');
+const { validateObjectId, validateMerchantIds } = require('../middleware');
 const {
     getAllCounters,
     getCounterById,
@@ -11,7 +11,7 @@ const {
 
 router.get("/", getAllCounters);
 router.get("/:id", validateObjectId, getCounterById);
-router.post("/", createCounter);
+router.post("/", validateMerchantIds, createCounter);
 router.put(":id", validateObjectId, updateCounter);
 router.delete("/:id", validateObjectId, deleteCounter);
 

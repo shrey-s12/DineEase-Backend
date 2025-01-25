36506af784c3659ffe10c501c73836b17cb9c9ca
Shrey Singhal <shreynbd12@gmail.com>
2025-01-25 18:16:49 +0530

    Populate data in post call in cartRouter

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -5,23 +5,21 @@ const User = require('../model/userModel');
 router.use(auth);
 
 router.get('/', async (req, res) => {
-    const cart = req.user.cart;
-    res.status(200).json(cart);
+    const user = req.user;
+    res.status(200).json(user);
 });
 
 router.post('/:dishId', async (req, res) => {
     req.user.cart.push({ dish: req.params.dishId, quantity: 1 });
-    const cart = req.user.cart;
-    await req.user.save();
-    res.status(200).json(cart);
+    await req.user.save().then(u => u.populate('cart.dish'));
+    res.status(200).json(req.user.cart);
 });
 
 router.patch('/:dishId', async (req, res) => {
     const item = req.user.cart.find(item => item.dish.id === req.params.dishId);
     item.quantity += req.body.changeQuantity;
-    const cart = req.user.cart;
     await req.user.save();
-    res.status(200).json(cart);
+    res.status(200).json(req.user.cart);
 });
 
 router.delete('/:dishId', async (req, res) => {

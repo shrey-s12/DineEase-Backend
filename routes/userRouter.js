7f2d1798e18ba9c490a4205d62279f3e34d6b0e5
Shrey Singhal <shreynbd12@gmail.com>
2025-01-28 21:32:17 +0530

    create a route for getting all the merchants

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,12 +4,14 @@ const { validateObjectId } = require('../middleware');
 const {
     getAllUsers,
     getUserById,
+    getMerchants,
     createUser,
     updateUser,
     deleteUser
 } = require('../controllers/userController');
 
 router.get("/", getAllUsers);
+router.get("/merchants", getMerchants);
 router.get("/:id", validateObjectId, getUserById);
 router.post("/", createUser);
 router.put("/:id", validateObjectId, updateUser)
